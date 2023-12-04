dee3287c5b533b6404a3d1ee1804ad3f90cd35db
ryan <ryan@rgskinner.com>
2023-12-03 21:54:03 -0800

    building better?

--- tools/fastbuild.bat
+++ tools/fastbuild.bat
@@ -1,6 +1,6 @@
 cmake -DCMAKE_BUILD_TYPE=Debug -DCMAKE_MAKE_PROGRAM=/tools/ninja.exe -DCMAKE_TOOLCHAIN_FILE=~/Documents/vcpkg/scripts/buildsystems/vcpkg.cmake -G Ninja -S .. -B ../cmake-build-debug
 
-cmake --build cmake-build-debug || (
+cmake --build ../cmake-build-debug || (
     echo Build failed.
     pause
     exit /b
